c0a3fd30bf6556b21f0b6e7a5bd2c2577b93c157
Sandeep <nanisandeep876@gmail.com>
2022-07-13 20:03:21 +0530

    create

---
+++ password.py
@@ -0,0 +1,24 @@
+
+# It imports the random module.
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$"
+
+# It creates a string of all the characters that can be used in the password.
+total = lower + upper + numbers + symbols
+
+len = 12
+# It creates a random password.
+password = "".join(random.sample(total, len))
+print(password)
+
+
+# Grouping the tuples by the second element in the tuple.
+t = [(2, 1), (1, 1), (2, 4), (2, 6), (7, 5), (6, 4)]
+result = {}
+for i, j in t:
+    result.setdefault(j, []).append(i)
+print(result)
