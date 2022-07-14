4bf7e1581699fc26848da241c347120cc2d3497a
Sandeep <nanisandeep876@gmail.com>
2022-07-14 19:17:52 +0530

    turtle 3

---
+++ turtle 3.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(60)
+bgcolor('black')
+color('orange')
+hideturtle()
+
+n=1
+p=True
+
+while True:
+    circle(n)
+    if p:
+        n=n-1
+    else:
+        n=n+1
+    
+    if n==0 or n==120:
+        p = not p
+    
+    left(1)
+    
+done()
